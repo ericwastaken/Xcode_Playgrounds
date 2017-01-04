5ab244a5372ec5ef65480115dfd607980e97954b
Eric A. Soto <eric@issfl.com>
2017-01-04 00:37:51 -0500

    Refactor GCD Example for clarity

--- Playgrounds/GCD Example.playground/Contents.swift
+++ Playgrounds/GCD Example.playground/Contents.swift
@@ -7,11 +7,12 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 var someValue: Int = 0
 
 // After 10 seconds, run this block in the global background queue
-DispatchQueue.global(qos:.background).asyncAfter(deadline: .now() + 5.0) {
+// For the time .milliseconds(Int), .seconds(Int), .microseconds(Int) and .nanoseconds(Int) may also be used.
+DispatchQueue.global(qos:.background).asyncAfter(deadline: .now() + .seconds(5)) {
     someValue = 10
     print("Global Background Queue -> someValue was set to: \(someValue)")
     // If we were doing some UI Operations, we'd jump back into the Main Queue
-    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
         someValue = 1
         print("Main Queue -> someValue was re-set to: \(someValue)")
     }
